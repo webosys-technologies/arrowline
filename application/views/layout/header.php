b6b8f7b2ac75df7337ab63fbe403c108bcdf2624
suraj_shinde\SURAJ <suraj@webosys.com>
2019-01-31 11:16:38 +0530

    lead status link chnage

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -689,10 +689,7 @@
                 if(in_array("manage_customer",$user_session)){?>
                 <li><a href="<?php echo base_url();?>Management/lead_customer"><i class="fa fa-user text-red"></i> <?php echo "Lead Customer";?> </a></li>
               <?php }} ?>
-                <?php if(isset($user_session)){
-                if(in_array("manage_customer",$user_session)){?>
-                <li><a href="<?php echo base_url();?>Management/lead_status"><i class="fa fa-user text-red"></i> <?php echo "Lead Status";?> </a></li>
-              <?php }} ?>
+               
 
            
               </ul>
@@ -970,6 +967,10 @@
                     </a>
                 </li>
                 <?php }} ?>
+                 <?php if(isset($user_session)){
+                if(in_array("manage_customer",$user_session)){?>
+                <li><a href="<?php echo base_url();?>Management/lead_status"><i class="fa fa-user text-red"></i> <?php echo "Add Lead Status";?> </a></li>
+              <?php }} ?>
 
 
                 <?php if(isset($user_session)){
